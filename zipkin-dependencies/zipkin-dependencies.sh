2060b67eb0c96625c6bb99e4cf0fae22ad7d84d4
gissily <gissily@outlook.com>
2022-10-19 11:24:44 +0800

    init zipkin-dependencies image build

---
+++ zipkin-dependencies/zipkin-dependencies.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+echo " "
+echo " "
+echo 'build zipkin-dependencies start'
+
+ZIPKIN_VERSION=$(curl https://api.github.com/repos/openzipkin/zipkin-dependencies/releases/latest | grep tag_name | cut -d '"' -f 4)
+
+docker build \
+    --build-arg ZIPKIN_VERSION=${ZIPKIN_VERSION} \
+    -t zipkin-dependencies:${ZIPKIN_VERSION} \
+    -f ${GITHUB_WORKSPACE}/zipkin-dependencies/Dockerfile . --no-cache
+docker image tag zipkin-dependencies:${ZIPKIN_VERSION} ${CI_REGISTRY}/opcal-project/containers/zipkin-dependencies:${ZIPKIN_VERSION}
+docker image tag zipkin-dependencies:${ZIPKIN_VERSION} ${CI_REGISTRY}/opcal-project/containers/zipkin-dependencies:latest
+docker push ${CI_REGISTRY}/opcal-project/containers/zipkin-dependencies:${ZIPKIN_VERSION}
+docker push ${CI_REGISTRY}/opcal-project/containers/zipkin-dependencies:latest
+
+echo 'build zipkin-dependencies finished'
+echo " "
+echo " "
